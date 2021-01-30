54cb160399f56a5cbe4f97468b7cd2d1385bc53c
Sky <776513284@qq.com>
2021-01-30 22:57:50 +0800

    第10章的作业

---
+++ src/com/daniu101/chapter10/hw1/Person.java
@@ -0,0 +1,54 @@
+package com.daniu101.chapter10.hw1;
+
+public class Person {
+    private String name;
+    private double height;
+    private int age;
+
+    public void eat() {
+        System.out.println("eat");
+    }
+
+    public void run() {
+        System.out.println("run");
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", age=" + age +
+                '}';
+    }
+
+    public Person(String name, double height, int age) {
+        this.name = name;
+        this.height = height;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/com/daniu101/chapter10/hw1/Test.java
@@ -0,0 +1,11 @@
+package com.daniu101.chapter10.hw1;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        Person person = new Person("Mike",180.30D, 23 );
+        System.out.println(person.toString());
+    }
+
+}
